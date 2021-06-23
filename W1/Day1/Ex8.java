b3a4c55eba3a602a2e3fa28c17b40864df193ec7
Vijayalakshmi-R09 <76153892+Vijayalakshmi-R09@users.noreply.github.com>
2021-06-23 16:56:28 +0530

    Add files via upload

---
+++ W1/Day1/Ex8.java
@@ -0,0 +1,23 @@
+package com.srm.javatraining;
+public class Ex8
+{
+public static void main (String[] args)
+    {
+    int n=Integer.parseInt(args[0]);
+    int ecount = 0;
+    int ocount = 0;
+    while (n > 0)
+    {
+        int rem = n % 10;
+        if (rem % 2 == 0)
+            ecount++;
+        else
+            ocount++;
+        n = n / 10;
+    }
+    System.out.println ( "Even count : " +
+                              ecount);
+    System.out.println ( "Odd count : " +
+                              ocount);
+}
+}
