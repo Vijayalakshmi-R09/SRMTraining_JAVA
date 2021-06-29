a76bddac56120eeef8e1c4b39f6bda5b537e932c
Vijayalakshmi-R09 <76153892+Vijayalakshmi-R09@users.noreply.github.com>
2021-06-29 16:18:26 +0530

    Update AppletDateTime.java

--- W1/Day7/AppletDateTime.java
+++ W1/Day7/AppletDateTime.java
@@ -9,44 +9,32 @@ import java.awt.Font;
 import java.awt.Color;
 import java.util.GregorianCalendar;
 
-public class AppletDateTime extends Applet implements Runnable
+public class AppletDateTime extends Applet 
 {
-	Thread t1,t2;
-	public void start()
-	{
-		t1=new Thread(this);
-		t1.start();
-	}
+	
 	public void init() 
                 {
                     setBackground(Color.blue);
                     setForeground(Color.white);
                     
                   }
-	public void run() {
-		t2=Thread.currentThread();
-		while(t2==t1)
-		{
-			repaint();
-			try {
-				t2.sleep(1000);
-			}
-			catch(InterruptedException ie)
-			{
-				System.out.println(ie);
-			}
-		}
-	}
+	
 	public void paint(Graphics g)
 	{
-                Font f = new Font ("TimesRoman", Font.BOLD, 30);
+                                  Font f = new Font ("TimesRoman", Font.BOLD, 30);
 		Calendar c=new GregorianCalendar();
 		Date d=c.getTime();
 		SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
-                g.setFont(f);
-                g.drawString("DATE AND TIMESTAMP: ",10,30);
+                                  g.setFont(f);
+                                  g.drawString("DATE AND TIMESTAMP: ",10,30);
 		g.drawString(sdf.format(d), 80, 80);
 	}
 }
+
+
+
+
+
+
 
 
