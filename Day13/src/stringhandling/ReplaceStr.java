0a453bab3e61a4d79ac19a9a72f4a34f9080b350
Vijayalakshmi-R09 <76153892+Vijayalakshmi-R09@users.noreply.github.com>
2021-07-06 16:56:43 +0530

    Update ReplaceStr.java

--- Day13/src/stringhandling/ReplaceStr.java
+++ Day13/src/stringhandling/ReplaceStr.java
@@ -9,7 +9,10 @@ public class ReplaceStr {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the string: ");
 		String s=sc.nextLine();
-		s=s.replace("a", "i");
-		System.out.println("the replaced string: "+s);
+		s=s.replace('i', 'e');
+		System.out.println("Replaced String('i'=>'e'): \n"+s);
+		StringBuffer sb = new StringBuffer("I is sane.");
+                sb.replace(2,4, "am");
+		System.out.println("\nUsing StringBuffer Replaced string: \n"+sb);
 	}
 }
